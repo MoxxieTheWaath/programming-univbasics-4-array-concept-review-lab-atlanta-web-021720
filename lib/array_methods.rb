f7977068abe9e02e1b466586ec3dab682ca1897b
Michael McDonald <mchlmcdnld7@gmail.com>
2020-02-13 00:07:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,8 +5,9 @@ def find_element_index(array, value_to_find)
     if array[counter] == value_to_find do
       return counter
     else
-      counter = counter + 1
+      /break
     end
+  end
 end
 
 def find_max_value(array)
